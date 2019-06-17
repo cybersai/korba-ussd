f451227356bb3fb28a49e6ab34718c7c6c012849
CyberSai <isaacsai030@gmail.com>
2019-06-17 14:41:07 +0100

    Cleaning project

--- src/service/Thanks.php
+++ src/service/Thanks.php
@@ -6,7 +6,7 @@ namespace Korba;
 
 class Thanks extends View
 {
-    public function __construct($success = false,$network = "INF")
+    public function __construct($network = "INF", $success = false)
     {
         if ($success) {
             $content = "Thanks, transaction completed Successfully";
