c6e345c44916d9365ae03781397fd079dea4a2c9
CyberSai <isaacsai030@gmail.com>
2019-06-25 11:57:58 +0100

    TvConfirmation Page

--- src/service/tv/TvConfirmationPage.php
+++ src/service/tv/TvConfirmationPage.php
@@ -9,7 +9,7 @@ class TvConfirmationPage extends View
     public function __construct($next, $number, $service, $name, $amount)
     {
         $fee = $amount * .01;
-        $content = "Confirm\nService:{$service}\nAcc No:{$number}\nName:{$name}\nAmount:{$amount}Network Fee: {$fee}\nTotal Debit: {$fee}";
+        $content = "Confirm\nService:{$service}\nAcc No:{$number}\nName:{$name}\nAmount:{$amount}\nNetwork Fee: {$fee}\nTotal Debit: {$fee}";
         parent::__construct($content, $next,1,null, null, null, "1.Ok\n#.Back\nMain Menu");
     }
 }
