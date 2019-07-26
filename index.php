53a709454bceca4e1dd7ab8a093cdea389d9100a
CyberSai <isaacsai030@gmail.com>
2019-07-26 11:37:21 +0100

    Dump classes

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ use Korba\SMS;
 
 require 'vendor/autoload.php';
 
-$sms = new SMS('https://gndvj.api.infobip.com', 'GaRuralBank','Test@12345', 'KorbaSMBTest');
+$sms = new SMS('https://gndvj.api.infobip.com', 'GaRuralBank','Test@12345', 'KorbaSMB');
 
-print_r($sms->send('Hello welcome to smb', ['0545112466', '0509460180', '0503369866']));
+print_r($sms->send('Hello welcome to smb', '0545112466'));
